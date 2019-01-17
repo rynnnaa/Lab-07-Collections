36fea03a41f79668d563060ab9c7611389c3d8b0
Tulip Pink <pinktulips@hotmail.com>
2019-01-16 21:56:41 -0800

    card class

--- Lab-07-Collections/Lab-07-Collections/Classes/Card.cs
+++ Lab-07-Collections/Lab-07-Collections/Classes/Card.cs
@@ -4,7 +4,19 @@ using System.Text;
 
 namespace Lab_07_Collections.Classes
 {
-    class Card
+    public class Card
     {
+        public string cardNum { get; set; }
+        public CardSuit CardSuit { get; set; }
+
+        public enum Suits { Heart, Diamond, Club, Spade }
+
+
+        public Card(string cardN, CardSuit cardsuit)
+        {
+            cardNum = cardN;
+            cardsuit = CardSuit;
+        }
+
     }
 }
--- Lab-07-Collections/Lab-07-Collections/Classes/Deck.cs
+++ Lab-07-Collections/Lab-07-Collections/Classes/Deck.cs
@@ -5,11 +5,4 @@ using System.Text;
 namespace Lab_07_Collections.Classes
 {
     class Deck
-    {
-        public string cardNum { get; set; }
-        public CardSuit CardSuit { get; set; }
 
-        public enum Suits { Heart, Diamond, Club, Spade }
-    }
-
-}
