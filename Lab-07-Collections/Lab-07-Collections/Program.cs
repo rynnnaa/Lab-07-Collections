cd7d77586f440fbb683ed2ab4e7f06fca3afad87
Tulip Pink <pinktulips@hotmail.com>
2019-01-16 21:45:50 -0800

    initial commit

---
+++ Lab-07-Collections/Lab-07-Collections/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lab_07_Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+    }
+}
