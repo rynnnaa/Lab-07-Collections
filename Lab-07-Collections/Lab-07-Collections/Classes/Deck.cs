b729fa02f54c04bd4b8b103840aef6b637cdfb3b
Tulip Pink <pinktulips@hotmail.com>
2019-01-16 22:08:03 -0800

    setting counter and iterate through deck

--- Lab-07-Collections/Lab-07-Collections/Classes/Deck.cs
+++ Lab-07-Collections/Lab-07-Collections/Classes/Deck.cs
@@ -1,8 +1,37 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Lab_07_Collections.Classes
 {
-    class Deck
+    public class Deck<T> : IEnumerable<T> where T : Card
+    {
+         T[] cards = new T[56];
+        int currentIndex = 0;
 
+        int counter = 0;
+        public void Add(T item)
+        {
+            if (currentIndex > cards.Length - 1)
+            {
+                Array.Resize(ref cards, cards.Length * 3);
+            }
+            cards[currentIndex] = item;
+            currentIndex++;
+            counter++;
+
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
