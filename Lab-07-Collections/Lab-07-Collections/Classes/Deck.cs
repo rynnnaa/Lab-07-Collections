b8d327854e86882f60f13afd750401419febf395
Tulip Pink <pinktulips@hotmail.com>
2019-01-16 22:12:54 -0800

    remove method logic

--- Lab-07-Collections/Lab-07-Collections/Classes/Deck.cs
+++ Lab-07-Collections/Lab-07-Collections/Classes/Deck.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Lab_07_Collections.Classes
 {
     public class Deck<T> : IEnumerable<T> where T : Card
     {
-         T[] cards = new T[56];
+        T[] cards = new T[56];
         int currentIndex = 0;
 
         int counter = 0;
@@ -25,9 +26,25 @@ namespace Lab_07_Collections.Classes
 
         public IEnumerator<T> GetEnumerator()
         {
-            for(int i = 0; i < currentIndex; i++)
+            for (int i = 0; i < currentIndex; i++)
             {
                 yield return cards[i];
+            }
+        }
+
+        public T[] Remove (T item)
+        {
+            for(int i =0; i <cards.Length; i++)
+            {
+                if (cards[i] == item)
+                {
+                    int index = Array.IndexOf(cards, item);
+                    counter--;
+
+                    T[] newCards = cards.Where((value,idx) => idx != index).ToArray();
+
+
+                }
             }
         }
 
