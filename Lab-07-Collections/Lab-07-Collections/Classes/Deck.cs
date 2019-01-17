06635b67dc4044f89069539e45601afb03c83317
Tulip Pink <pinktulips@hotmail.com>
2019-01-16 21:52:38 -0800

    made card and deck class

---
+++ Lab-07-Collections/Lab-07-Collections/Classes/Deck.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_07_Collections.Classes
+{
+    class Deck
+    {
+        public string cardNum { get; set; }
+        public CardSuit CardSuit { get; set; }
+
+        public enum Suits { Heart, Diamond, Club, Spade }
+    }
+
+}
