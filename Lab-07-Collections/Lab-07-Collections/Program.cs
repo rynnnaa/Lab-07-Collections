a159761e433f774e30dc8fa42d1fb3a1e0e88183
Tulip Pink <pinktulips@hotmail.com>
2019-01-22 19:19:33 -0800

    Adding cards

--- Lab-07-Collections/Lab-07-Collections/Program.cs
+++ Lab-07-Collections/Lab-07-Collections/Program.cs
@@ -6,19 +6,28 @@ namespace Lab_07_Collections
     public class Program
     {
         public static object Deck { get; set; }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Cards In Deck:");
+            Console.WriteLine("");
+
+            //set up deck 
 
             Deck<Card> Deck = new Deck<Card>();
 
+            Card card1 = new Card();
+            card1.Value = Card.CardValue.Ace;
+            card1.suit = Card.Suit.Clubs;
+
+            Card card2 = new Card();
+            card2.Value = Card.CardValue.Two;
+            card2.suit = Card.Suit.Diamonds;
 
 
-            foreach (Card item in Deck)
-            {
-                Console.Write($"{item.cardNum} {item.cardsuit}");
-                
-            }
+
+
+
             
         }
     }
