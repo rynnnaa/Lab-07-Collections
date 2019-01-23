58faa0fbf6552fffc0cfaec398b25917be4e3873
Tulip Pink <pinktulips@hotmail.com>
2019-01-22 19:22:49 -0800

    add cards

--- Lab-07-Collections/Lab-07-Collections/Program.cs
+++ Lab-07-Collections/Lab-07-Collections/Program.cs
@@ -24,11 +24,32 @@ namespace Lab_07_Collections
             card2.Value = Card.CardValue.Two;
             card2.suit = Card.Suit.Diamonds;
 
+            Card card3 = new Card();
+            card3.Value = Card.CardValue.Three;
+            card3.suit = Card.Suit.Hearts;
 
+            Card card4 = new Card();
+            card4.Value = Card.CardValue.Four;
+            card4.suit = Card.Suit.Spades;
 
+            Card card5 = new Card();
+            card5.Value = Card.CardValue.Five;
+            card5.suit = Card.Suit.Diamonds;
 
+            Card card6 = new Card();
+            card6.Value = Card.CardValue.Six;
+            card6.suit = Card.Suit.Hearts;
 
-            
+            Card card7 = new Card();
+            card7.Value = Card.CardValue.Seven;
+            card7.suit = Card.Suit.Clubs;
+
+            Card card8 = new Card();
+            card8.Value = Card.CardValue.Eight;
+            card8.suit = Card.Suit.Diamonds;
+
+   
+
         }
     }
 }
