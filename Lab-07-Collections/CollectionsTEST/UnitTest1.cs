580063a817abe5643b19ae4cacd5a9d840e52bdf
Tulip Pink <pinktulips@hotmail.com>
2019-01-22 19:38:03 -0800

    remove card

--- Lab-07-Collections/CollectionsTEST/UnitTest1.cs
+++ Lab-07-Collections/CollectionsTEST/UnitTest1.cs
@@ -20,5 +20,39 @@ namespace CollectionsTEST
 
 
         }
+
+        [Fact]
+        public void GetSetTest()
+        {
+            Card card2 = new Card();
+            card2.Value = Card.CardValue.Two;
+            card2.CardSuit = Card.Suit.Diamonds;
+            var Diamonds = card2.suit;
+
+            Deck<Card> Deck = new Deck<Card>();
+            Deck.Add(card2);
+
+            Assert.Equal(Diamonds, card2.suit);
+        }
+
+        [Fact]
+        public void RemoveCardTest()
+        {
+            Card card3 = new Card();
+            card3.Value = Card.CardValue.Three;
+            card3.suit = Card.Suit.Hearts;
+
+            Card card4 = new Card();
+            card4.Value = Card.CardValue.Four;
+            card4.suit = Card.Suit.Spades;
+
+            Deck<Card> Deck = new Deck<Card>();
+            Deck.Add(card3);
+            Deck.Add(card4);
+
+            Deck.Remove(card4);
+
+            Assert.DoesNotContain(card4, Deck);
+        }
     }
 }
